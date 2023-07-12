cffb7db441d865cb53386bb5ec1b03a395eb006d
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2023-07-12 10:33:36 -0500

    Done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Class4Review
+﻿using System.Globalization;
+
+namespace Class4Review
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            MySpace.Class1 MyClass = new MySpace.Class1();
+            Console.Write(MyClass.students.Length);
+            Console.Write("\n");
+            MyClass.students[0] = "Molly";
+            Console.Write("The #1 student is " + MyClass.students[0]);
         }
     }
 }
